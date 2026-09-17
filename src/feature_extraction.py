00ed58ea23eca7c57224581e86b4b51cc570a82b
Karim <karimsalahie@gmail.com>
2026-09-17 10:12:36 -0400

    Update feature_extraction.py

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -2,10 +2,10 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 
-def select_top_features(X, y):
+def top_features(X, y):
     '''
-    Uses a Random Forest classifier to determine the most predictive
-    gait variables based on Gini importance.
+    Random Forest used to select the most predictive
+    gait variables based on importance from the data.
     '''
     rf_selector = RandomForestClassifier(n_estimators=100, random_state=42)
     rf_selector.fit(X, y)
